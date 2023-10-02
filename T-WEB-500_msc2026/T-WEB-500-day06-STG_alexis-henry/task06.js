417b7afb1c22d20d3792333a34171ddbe7cfb104
AlexisHenry <alexis.henry150357@gmail.com>
2023-10-02 11:02:24 +0200

    Push T-WEB-500-day06-STG_alexis-henry.

---
+++ T-WEB-500_msc2026/T-WEB-500-day06-STG_alexis-henry/task06.js
@@ -0,0 +1,11 @@
+export function objectsDeeplyEqual(a, b) {
+		if (a === b) return true;
+		if (a == null || typeof a != "object" ||
+				b == null || typeof b != "object") return false;
+		let keysA = Object.keys(a), keysB = Object.keys(b);
+		if (keysA.length != keysB.length) return false;
+		for (let key of keysA) {
+				if (!keysB.includes(key) || !objectsDeeplyEqual(a[key], b[key])) return false;
+		}
+		return true;
+}
