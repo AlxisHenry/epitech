d21217d84d00a4cc0efd1b32c263eb491f8c22e0
AlexisHenry <alexis.henry150357@gmail.com>
2023-10-02 12:01:32 +0200

    Delete T-WEB-500-day06-STG_alexis-henry.

--- T-WEB-500_msc2026/T-WEB-500-day06-STG_alexis-henry/task07.js
+++
@@ -1,9 +0,0 @@
-export function arrayFiltering(array, test) {
-	let newArray = [];
-	for (let i = 0; i < array.length; i++) {
-		if (test(array[i])) {
-			newArray.push(array[i]);
-		}
-	}
-	return newArray;
-}
