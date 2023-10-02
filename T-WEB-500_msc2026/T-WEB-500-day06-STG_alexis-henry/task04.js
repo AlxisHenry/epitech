557b65d434a1500f3c73514156b5af605e6d964f
AlexisHenry <alexis.henry150357@gmail.com>
2023-10-02 12:40:20 +0200

    Push T-WEB-500-day06-STG_alexis-henry.

---
+++ T-WEB-500_msc2026/T-WEB-500-day06-STG_alexis-henry/task04.js
@@ -0,0 +1,16 @@
+export function fizzBuzz(num) {
+	let output = []
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      output.push("Fizz");
+    } else if (i % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(i);
+    }
+  }
+  console.log(output.join(", "));
+  return 0;
+}
