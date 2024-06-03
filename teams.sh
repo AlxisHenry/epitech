8a2d1c6c85bddd33fccd723c4e8d9467acb5733f
Alexis <91117127+AlxisHenry@users.noreply.github.com>
2024-06-03 15:41:19 +0200

    fix: teams separators

--- teams.sh
+++ teams.sh
@@ -19,7 +19,7 @@ function makeTeamsImageUrl() {
         url="$TEAMS_API_URL"
 
         for team in ${!teamCount[@]}; do
-                url+="${team}-${teamCount[$team]}@"
+                url+="${team}@${teamCount[$team]},"
         done
 
         url=${url::-1}
