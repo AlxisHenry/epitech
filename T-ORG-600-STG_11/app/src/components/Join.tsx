ec516c5b71f1cf7d33e76a9dcffe44bc08837cee
AlxisHenry <alexis.henry150357@gmail.com>
2024-10-17 15:44:00 +0200

    fix: test to remove T_ORG_600

--- T-ORG-600-STG_11/app/src/components/Join.tsx
+++
@@ -1,3 +0,0 @@
-export function Join() {
-  return <div className={"join"}></div>;
-}
--- T-ORG-600-STG_11/app/src/mocks/index.ts
+++
@@ -1,7 +0,0 @@
-import charts from "./../../charts.json";
-
-import { Chart } from "../types";
-
-export function getCharts(): Chart[] {
-  return charts;
-}
