18ae012042f3a548b4fe65cbd61de5aad9d402df
AlxisHenry <alexis.henry150357@gmail.com>
2026-01-23 16:33:34 +0100

    chore: remove teams usage

--- Makefile
+++ Makefile
@@ -1,6 +1,3 @@
 .PHONE: clean
 clean:
 	@find ./* -type f -name "*.Identifier" -exec rm -f {} \;
-
-teams:
-	bash ./teams.sh
--- sync.sh
+++ sync.sh
@@ -92,39 +92,6 @@ function chooseTeam() {
 	fi
 }
 
-function makeTeamsImageUrl() {
-	folders="$(ls -d T-*/)"
-	teams=()
-
-	for folder in $folders; do
-		if [[ ! $folder =~ $PROJECT_EXTENSION ]]; then
-			team=$(echo $folder | cut -d'-' -f4- | cut -d'/' -f1)
-			teams+=("$team")
-		fi
-	done
-
-	declare -A teamCount
-	for team in ${teams[@]}; do
-		teamCount[$team]=$((${teamCount[$team]} + 1))
-	done
-
-	url="$TEAMS_API_URL"
-
-	for team in ${!teamCount[@]}; do
-		url+="${team}@${teamCount[$team]},"
-	done
-
-	url=${url::-1}
-	currentLine=$(grep -n "Teams](" README.md | cut -d':' -f1)
-	if [[ -n "$currentLine" ]]; then
-		sed -i "${currentLine}s|.*|![Teams](${url})|" README.md
-		git add README.md
-		git commit -m "Update Teams image in README.md"
-	else
-		echo "No line with 'Teams](' found in README.md"
-	fi
-}
-
 if [ "${1}" == "--help" ] || [ "${1}" == "-h" ]; then
 	help
 	exit
@@ -225,7 +192,6 @@ fi
 
 if [ ${hasCommit} == true ]; then
 	make clean >/dev/null 2>&1
-	makeTeamsImageUrl >/dev/null 2>&1
 	git push >/dev/null 2>&1
 	echo -e "\n\e[0;32mSuccess\e[0m: Pushed to remote.\n"
 fi
